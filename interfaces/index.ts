ea3ad233c5fbfa058941de3486594001e6cc45ed
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2025-11-20 06:40:13 -0800

    add all files

---
+++ interfaces/index.ts
@@ -0,0 +1,16 @@
+// interfaces/index.ts
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface PageRouteProps {
+  pageRoute: string;
+}
