3b3a787b400ace6de0644f6ccff424360b9c5c66
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-11-21 02:44:35 -0800

    Update hello.ts

--- alx-project-0x03/pages/api/hello.ts
+++ alx-project-0x03/pages/api/hello.ts
@@ -1,4 +1,4 @@
-l// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
